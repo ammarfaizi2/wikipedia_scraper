cf0d4be8d22eec9c563892c9d4157582fe3101dd
Ammar Faizi <ammarfaizi2@gmail.com>
2018-02-03 01:06:59 +0700

    Update require file

--- main.php
+++ main.php
@@ -1,12 +1,12 @@
 <?php
 
-// require __DIR__."/src/Wikipedia.php";
-require __DIR__."/vendor/autoload.php";
+require __DIR__."/src/Wikipedia.php";
+// require __DIR__."/vendor/autoload.php";
 
 define("WIKIPEDIA_DATA_DIR", __DIR__."/data");
 
 $query = $argv[1];
-$wiki = new Wikipedia\Wikipedia($query);
+$wiki = new Wikipedia\Wikipedia($query, "en");
 $wiki = $wiki->search();
 
 var_dump($wiki);
